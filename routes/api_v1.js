9d32a3f3428d63a4581219efc9ae62713f6d49b1
Luiserebii <luis@serebii.io>
2019-02-25 00:48:33 -0500

    Great, working API at /v1/git diff --cachedgit diff --cached

--- routes/api_v1.js
+++ routes/api_v1.js
@@ -1,7 +1,43 @@
 const express = require('express');
+const MathFun = require('../src/math');
+
 
 var router = express.Router();
 
 
+/*
+ * API STUFF
+ *
+ */
+
+
+router.get("/fibonacci", function(req, res, next){
+   if(req.query.type) { return next(); }
+   if(!req.query.n) { return res.end("ERROR: No \"n\" parameter query passed."); }
+
+   let n = parseInt(req.query.n, 10);
+   let fibResult = MathFun.fibonacci();
+  
+   let jsonRes = { result: fibResult, success: 200 };
+   res.json(jsonRes);
+});
+
+router.use(function(req, res, next){
+   if(!req.query.type == "isFib") { return next(); }
+   if(!req.query.num) { return res.end("ERROR: No \"fib\" parameter query passed."); }
+
+   let num = parseInt(req.query.num, 10);
+   let boolRes = MathFun.isFibonacci(num);
+
+   let jsonRes = { result: boolRes, success: 200 }
+   res.json(jsonRes);
+});
+
+
+
+
+router.use(function(req, res){
+   res.end("Sorry, nothing hit!");
+});
 
 module.exports = router;
